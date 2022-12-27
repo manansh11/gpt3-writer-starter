a643cc3c166fd40310436f43884c75746b60ed4b
Manansh Shukla <47335622+manansh11@users.noreply.github.com>
2022-12-27 01:33:30 +0000

    Added prompts

---
+++ pages/api/generate.js
@@ -0,0 +1,47 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+const basePromptPrefix = `
+We are currently in conversation with the Hindu god Krishna. He gives detailed, and enlightened answers to every question. His answers are similar to the answers he gave Arjuna on the battlefield of Kurukshetra
+Me: 
+`;
+const generateAction = async (req, res) => {
+
+    // run first prompt
+    //console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+        temperature: 0.9,
+        max_tokens: 1000,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+
+    const secondPrompt = `
+    Given a reply from the God Krishna, create a approachable, step-by-step lesson that anyone could understand that helps solve or explain the answer. The answer is: 
+    `
+
+    const secondPromptCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${secondPrompt}${basePromptOutput.text}`,
+        temperature: 0.9,
+        max_tokens: 1000,
+    });
+
+    const secondPromptOutput = secondPromptCompletion.data.choices.pop()
+
+    const toSend = basePromptOutput;
+
+    res.status(200).json({output: toSend})
+
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,76 @@ import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
+import { useState } from 'react';
+
+
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    console.log('CALLING OPENAI...');
+
+    const respose = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({userInput}),
+    });
+
+    const data = await respose.json(); // convert response to json
+    const { output } = data; // pull out output
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`)
+    setIsGenerating(false);
+  }
+
+
+  const onUserChangedText = (e) => {
+    setUserInput(e.target.value);
+  }
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 WRITER MANANSH</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Krishna Talk:</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask questions, Krishna knows all.</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className='prompt-container'>
+          <textarea placeholder='ask your question here...' className='prompt-box' value={userInput} onChange={onUserChangedText}/>
+        </div>
+        <div className='prompt-buttons'>
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+            <div className='generate'>
+              {isGenerating ? <span className='loader'></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+          <div className='output'>
+            <div className='output-header-container'>
+              <div className='output-header'>
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className='output-content'>
+              <p>{apiOutput}</p>
+            </div>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
